b21c61c9726a1fbaff4cfc1d38771fd7e157285b
Vladislav A. Zaytsev <vestild@gmail.com>
2020-01-03 01:48:46 +0300

    move mask secret to other mod

--- src/main.rs
+++ src/main.rs
@@ -5,48 +5,19 @@ extern crate lazy_static;
 
 use simplelog::*;
 use log::{info};
-use tokio::run;
 use futures::future::lazy;
 
 mod config;
 mod init;
 mod server_config;
+mod mask_secret;
 
-fn main() {
+#[tokio::main(basic_scheduler)]
+async fn main() {
     SimpleLogger::init(LevelFilter::Debug, Config::default()).unwrap();
 
-    info!(target: "main", "token {:?}", format_secret(config::CONF.token()));
+    info!(target: "main", "token {:?}", mask_secret::mask(config::CONF.token()));
     let sc = server_config::ConfigProvider::new();
 
-    run(lazy(|| {
-        init::run(sc)
-    }));
-}
-
-fn format_secret(s: &str) -> String {
-    let mut r = String::from(s);
-    let len = s.len();
-    if len < 3 {
-        return String::from("**");
-    } else if len <= 6 {
-        r.replace_range(1.., &"*".repeat(len - 1))
-    } else {
-        r.replace_range(2..len - 2, &"*".repeat(len - 4))
-    }
-    r
-}
-
-#[cfg(test)]
-mod test{
-    use super::*;
-    
-    #[test]
-    fn test_log_secret() {
-        assert_eq!(format_secret(""), "**");
-        assert_eq!(format_secret("1"), "**");
-        assert_eq!(format_secret("123"), "1**");
-        assert_eq!(format_secret("123456"), "1*****");
-        assert_eq!(format_secret("1234567"), "12***67");
-        assert_eq!(format_secret("123456789"), "12*****89");
-    }
+    init::run(sc)
 }
---
+++ src/mask_secret.rs
@@ -0,0 +1,27 @@
+pub fn mask(s: &str) -> String {
+    let mut r = String::from(s);
+    let len = s.len();
+    if len < 3 {
+        return String::from("**");
+    } else if len <= 6 {
+        r.replace_range(1.., &"*".repeat(len - 1))
+    } else {
+        r.replace_range(2..len - 2, &"*".repeat(len - 4))
+    }
+    r
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn test_log_secret() {
+        assert_eq!(mask(""), "**");
+        assert_eq!(mask("1"), "**");
+        assert_eq!(mask("123"), "1**");
+        assert_eq!(mask("123456"), "1*****");
+        assert_eq!(mask("1234567"), "12***67");
+        assert_eq!(mask("123456789"), "12*****89");
+    }
+}
